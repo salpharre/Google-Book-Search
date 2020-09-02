3dad00991b2fbe2e9106705624bf212e4334f46a
Sandra <sandra.alpha2010@gmail.com>
2020-09-01 18:13:57 -0700

    changed mongoose connect options

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 // Start the API server
 app.listen(PORT, function() {
